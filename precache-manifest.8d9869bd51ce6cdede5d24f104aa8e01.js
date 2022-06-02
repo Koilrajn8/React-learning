04e2b949dfd7f17593dae91bf7b02da83a64af6b
koilrajn8 <koilrajn8@gmail.com>
2022-06-02 12:58:20 +0530

    Updates

---
+++ precache-manifest.8d9869bd51ce6cdede5d24f104aa8e01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "866accd86dfa0050b004",
+    "url": "/static/css/main.e1a15461.chunk.css"
+  },
+  {
+    "revision": "866accd86dfa0050b004",
+    "url": "/static/js/main.866accd8.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "a7ba6b7fe9cb522fab82",
+    "url": "/static/js/2.a7ba6b7f.chunk.js"
+  },
+  {
+    "revision": "ae42267ff6069bcb7427",
+    "url": "/static/js/runtime~main.ae42267f.js"
+  },
+  {
+    "revision": "25c2b503117253a22c4a9f33ea32f083",
+    "url": "/index.html"
+  }
+];
